b459d954ee6de6a18b50e2e03705a517ac41cf1b
Mukhriddin <mshakhriyorov8@gmail.com>
2021-09-21 18:34:50 +0500

    first day

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import "antd/dist/antd.css";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React world!!!</h1>
+      <BrowserRouter>
+        <div className="header">
+          <NavLink exact activeClassName="active"  to="/" >Home</NavLink>
+          <NavLink activeClassName="active"  to="/login" >Login</NavLink>
+          <NavLink activeClassName="active"  to="/dashboard" >Dashboard</NavLink>
+        </div>
+        <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
