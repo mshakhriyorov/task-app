891b0aa82bc79e4d6160d04e347e85dcb61b4cba
Mukhriddin <mshakhriyorov8@gmail.com>
2021-09-24 18:22:50 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import Home from './Home';
-import Login from './Login';
-import Dashboard from './Dashboard';
 import "antd/dist/antd.css";
+import { useAuth0 } from '@auth0/auth0-react';
+import "antd/dist/antd.css";
+import './App.css';
 
 
-function App() {
+const App = () => {
+  const { loginWithRedirect, isAuthenticated, logout, isLoading } = useAuth0();
+
+  if(isLoading) return <div className="loading"><h1>Loading...</h1></div>
+
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="header">
-          <NavLink exact activeClassName="active"  to="/" >Home</NavLink>
-          <NavLink activeClassName="active"  to="/login" >Login</NavLink>
-          <NavLink activeClassName="active"  to="/dashboard" >Dashboard</NavLink>
+        <div className="header" style={{display:'flex', alignItems:'center', justifyContent: 'space-between'}}>
+          <div style={{display:'flex', alignItems:'center'}}>
+            <NavLink exact activeClassName="active"  to="/" >Home</NavLink>
+          </div>
+         { !isAuthenticated && (<NavLink activeClassName="active"  to="/login" onClick={loginWithRedirect}  className="login">Login</NavLink>)}
+         { isAuthenticated && (<NavLink activeClassName="active"  to="/logout" onClick={logout} className="logout">Log Out</NavLink>)}
         </div>
         <div className="content">
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/login" component={Login} />
-              <Route path="/dashboard" component={Dashboard} />
             </Switch>
         </div>
       </BrowserRouter>
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Typography, Layout, Menu, Input, Image } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined
+} from '@ant-design/icons';
+import "antd/dist/antd.css";
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import { IIFC } from "react-iifc";
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <>
-    <h1>Hello welcome home :|</h1>
-    </>
-  )
+
+const { Title } = Typography;
+const { Header, Sider, Content } = Layout;
+const { Search } = Input;
+
+class Home extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <IIFC>
+        {() => {
+          const { user, isAuthenticated } = useAuth0();
+
+          const [users, setUsers] = useState([]);
+
+          useEffect(() => {
+            axios.get('https://jsonplaceholder.typicode.com/users')
+              .then(response => {
+                setUsers(response.data);
+              })
+              .catch(error => {
+                console.log(error);
+              })
+          })
+
+          function onSearch() {
+
+          }
+
+          if (!isAuthenticated) return (
+            <Title>Hello, welcome to Task Application!</Title>
+          )
+
+          return (
+            <Layout>
+              <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                  <Menu.Item key="1">
+                    <Image src={user.picture} />
+                    {user.name}
+                  </Menu.Item>
+                </Menu>
+              </Sider>
+              <Layout className="site-layout">
+                <Header className="site-layout-background header" style={{ padding: 0 }}>
+                  {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                    className: 'trigger',
+                    onClick: this.toggle,
+                  })}
+                  <Search placeholder="Search..." onSearch={onSearch} style={{ width: 200 }} />
+                </Header>
+                <Content
+                  className="site-layout-background"
+                  style={{
+                    margin: '24px 16px',
+                    padding: 24,
+                    minHeight: 280,
+                  }}
+                >
+                  
+                </Content>
+              </Layout>
+            </Layout>
+          )
+        }}
+      </IIFC>
+    );
+  }
 }
 
 export default Home;
